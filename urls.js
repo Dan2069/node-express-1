da3c9e46167905c1358b178fdf20dde1b8d4cf4b
Daniel <danielmosqueda15@gmail.com>
2022-08-24 05:04:34 -0400

    Completed Part 1 and Part 2

--- urls.js
+++ urls.js
@@ -3,86 +3,56 @@ const process = require('process');
 const axios = require('axios');
 
 
-// function handleOutput(text, out) {
-//     if (out) {
-//         console.log('Line 8');
-//         fs.writeFile(out, text, 'utf8', function(err){
-//             console.log("line 10");
-//             console.log(out);
-//             if (err) {
-//                 console.error(`Couldn't write ${out}: ${err}`);
-//                 process.exit(1);
-//             }
-//         });
-//     }
-//     else {
-//         console.log('Line 17');
-//         console.log(text);
 
-//     }
-// }
+function cat(path) {
+    fs.readFile(path, 'utf8', function(err, data){
+        if (err){
+            console.error(`Error reading ${path}: ${err}`);
+            process.exit(1);
+        }
+        else {
+            // console.log(data);
+            let exmpl = data.toString().split("\n"); //takes the four links and turns it into a string and separates them with \n
+            for(i in exmpl){ // for every link in teh exmpl, we're going to URL each of them and get their hostname out of it.
+                const { href, hostname} = new URL(exmpl[i]) // The hostname will be the name for the txt files. "hostname".txt
+                // console.log(href, hostname);
+                // console.log("Line 21");
+                webCat(href, hostname);
+            };
+        }
+    });
+}
+
+async function webCat(url, hostname) {
+    try {
+        let resp = await axios.get(url);
+        // console.log(resp.data);
+        handleOutput(resp.data, hostname);
+        console.log(`Wrote to ${hostname}`);
+    }
+    catch (err) {
+        // console.log(`Error fetching ${url}: ${err}`);
+        // process.exit(1);
+        console.log(`Couldn't download ${url}`);
+    }
+}
+
+function handleOutput(body, output){
+    if (output){
+        fs.writeFile(output, body, 'utf8', function(err){
+            if (err){
+                console.log(`Couldn't write ${output}: ${err}`);
+                process.exit(1);
+            }
+        })
+    }
+    else {
+        console.log("Line 59");
+    }
+}
 
 
 
-// console.log('Line 23');
-
-// function cat(path, out) {
-//     fs.readFile(path, 'utf8', function(err, data) {
-//         console.log("Line 27");
-//         if (err) {
-//             console.error(`Error reading ${path}: ${err}`);
-//             process.exit(1);
-//         }
-//         else {
-//             console.log("Line 33");
-//             const lines = data.split(/\r?\n/);
-//             for(let y = 0; y < 4; y++){
-//                 let link = new URL(lines[y]);
-//                 let domain = link.hostname
-//                 // console.log(link);
-//                 // console.log(domain);
-//                 // console.log("Line 38");
-//                 let out = domain + '.txt';
-//             handleOutput(lines[y],out)};
-//         }
-//     });
-// }
-
-// // console.log("Line 33");
-
-// // console.log("Line 56");
 
 
-// async function webCat(url, out) {
-//     try {
-//         let resp = await axios.get(url);
-//         handleOutput(resp.data, out)
-//         console.log("Line 46");
-//     }
-//     catch (err) {
-//         console.log(`Error fetching ${url}: ${err}`);
-//         process.exit(1);
-//     }
-// }
-// // console.log("Line 53");
-// let path;
-// let out;
-
-// // if (process.argv[2] === '--out') {
-// //     console.log("Line 58");
-// //     out = process.argv[3];
-// //     path = process.argv[4];
-// // }
-// // else{
-// //     console.log("Line 63");
-// //     path = process.argv[2];
-// // }
-
-// // if (path.slice(0,4) === 'http') {
-// //     console.log("Line 68");
-// //     webCat(path, out);
-// // }
-// // else {
-// //     console.log("Line 72");
-// //     cat(path, out);
-// // }
+cat(process.argv[2]);
